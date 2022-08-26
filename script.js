17e4d431c2053f7630b61b3d119fbaff764d3093
wictorianx <88106599+wictorianx@users.noreply.github.com>
2022-08-27 00:57:40 +0300

    done for now

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-document.addEventListener("DOMContent")
+const squares = document.querySelectorAll(".grid div")
+const result = document.querySelector("#result")
+const dCurrentPlayer = document.querySelector("#current-player")
+let currentPlayer = 1
+
+function checkBoard(){
+    
+}
+for (let i = 0; i<squares.length;i++){
+    squares[i].onclick=()=>{
+        if(squares[i+7].classList.contains("taken")){
+            if (currentPlayer==1){
+                squares[i].classList.add("taken")
+                squares[i].classList.add("player-one")
+                currentPlayer=2
+                dCurrentPlayer.innerHTML =currentPlayer
+            }
+            if (currentPlayer==2){
+                squares[i].classList.add("taken")
+                squares[i].classList.add("player-two")
+                currentPlayer=1
+                dCurrentPlayer.innerHTML =currentPlayer
+            }
+            else{alert("cant go here");checkBoard()}
+            
+        }
+    }
+}
